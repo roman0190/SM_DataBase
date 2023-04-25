a93872a5a54670e320762b543840592a6a934d14
Mrittika, Wasifa Tasnim <21-45108-2@student.aiub.edu>
2023-04-26 00:06:59 +0600

    yooo

--- Tables/dbo.login_table.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[login_table] (
-  [username] [varchar](50) NOT NULL,
-  [password] [varchar](50) NULL,
-  [type] [varchar](50) NULL,
-  CONSTRAINT [PK_login_table] PRIMARY KEY CLUSTERED ([username])
-)
-ON [PRIMARY]
-GO
