2129cfd97d3f555e76ad3b6b52a00cabe470b142
HOWLADAR, ROMAN <21-45129-2@student.aiub.edu>
2023-04-26 00:36:32 +0600

    dilam

---
+++ Tables/dbo.login_table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[login_table] (
+  [username] [varchar](50) NOT NULL,
+  [password] [varchar](50) NULL,
+  [type] [varchar](50) NULL,
+  CONSTRAINT [PK_login_table] PRIMARY KEY CLUSTERED ([username])
+)
+ON [PRIMARY]
+GO
