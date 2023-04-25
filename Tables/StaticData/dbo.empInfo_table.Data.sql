8b4fafde71d83a11d086cf33163487aed7e2e93b
HOWLADAR, ROMAN <21-45129-2@student.aiub.edu>
2023-04-26 02:44:25 +0600

    hi dada

---
+++ Tables/StaticData/dbo.empInfo_table.Data.sql
@@ -0,0 +1,3 @@
+﻿INSERT dbo.empInfo_table(username, password, role) VALUES ('abdulla', '123', 'emp');
+INSERT dbo.empInfo_table(username, password, role) VALUES ('alvi', '234', 'emp');
+INSERT dbo.empInfo_table(username, password, role) VALUES ('mitti', '123', 'emp');
