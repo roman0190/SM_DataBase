4026b726acacd948c1c18deb17fa6f2915fc30d3
Abdulla-Bin-Islam <21-45122-2@student.aiub.edu>
2023-04-26 21:12:16 +0600

    Category Table added

---
+++ Tables/StaticData/dbo.Eggs.Data.sql
@@ -0,0 +1,3 @@
+﻿INSERT dbo.Eggs(ID, Product, Price, Quantity) VALUES (1, N'White egg', 140, 12);
+INSERT dbo.Eggs(ID, Product, Price, Quantity) VALUES (2, N'Brown egg', 134, 12);
+INSERT dbo.Eggs(ID, Product, Price, Quantity) VALUES (3, N'Koil egg', 60, 12);
---
+++ Tables/StaticData/dbo.Rice.Data.sql
@@ -0,0 +1,6 @@
+﻿INSERT dbo.Rice(ID, Product, Price, Quantity) VALUES (N'1', N'Basmoti', N'650', N'1');
+INSERT dbo.Rice(ID, Product, Price, Quantity) VALUES (N'2', N'Aush', N'270', N'1');
+INSERT dbo.Rice(ID, Product, Price, Quantity) VALUES (N'3', N'Boro', N'390', N'1');
+INSERT dbo.Rice(ID, Product, Price, Quantity) VALUES (N'4', N'Sugondhi', N'220', N'1');
+INSERT dbo.Rice(ID, Product, Price, Quantity) VALUES (N'5', N'Najir shi', N'160', N'1');
+INSERT dbo.Rice(ID, Product, Price, Quantity) VALUES (N'6', N'Paijam', N'170', N'1');
