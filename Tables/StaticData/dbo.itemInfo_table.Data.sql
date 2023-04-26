87f686bc733e87f6a4f61c3c9b49aebf22397f8f
Mrittika, Wasifa Tasnim <21-45108-2@student.aiub.edu>
2023-04-26 19:57:23 +0600

    pushing againnnn

--- Tables/StaticData/dbo.itemInfo_table.Data.sql
+++ Tables/StaticData/dbo.itemInfo_table.Data.sql
@@ -1,3 +1,5 @@
 ﻿INSERT dbo.itemInfo_table(ID, Name, Quantity, Price, Category) VALUES (2, 'Chicken', '3', 600, 'Meat');
 INSERT dbo.itemInfo_table(ID, Name, Quantity, Price, Category) VALUES (3, 'chili powder', '6', 220, 'Spices');
 INSERT dbo.itemInfo_table(ID, Name, Quantity, Price, Category) VALUES (4, 'Tomatoes', '3', 180, 'Vegetable');
+
+INSERT dbo.itemInfo_table(ID, Name, Quantity, Price, Category) VALUES (5, 'Beef', '1', 700, 'Meat');
