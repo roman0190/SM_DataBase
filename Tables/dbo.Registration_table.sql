4533496aa74682f0d2375f8d0c4d3f8f4811a89e
Hossain, Musharuf <21-45112-2@student.aiub.edu>
2023-04-28 03:24:14 +0600

    Yahoo!

---
+++ Tables/dbo.Registration_table.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Registration_table] (
+  [Username] [nvarchar](50) NOT NULL,
+  [Password] [nvarchar](50) NOT NULL,
+  [First_name] [nvarchar](50) NOT NULL,
+  [Last_name] [nvarchar](50) NOT NULL,
+  [Email] [nvarchar](50) NOT NULL,
+  [Gender] [nvarchar](50) NOT NULL,
+  [Address] [nvarchar](50) NOT NULL,
+  CONSTRAINT [PK_Registration_table] PRIMARY KEY CLUSTERED ([Username])
+)
+ON [PRIMARY]
+GO
